8841e007f47e9b85145398e977bae3b64e81b2c9
Mohan51502 <rajmohan51502@gmail.com>
2023-04-05 09:15:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import './App.css';
+import TeamDetails from './pages/TeamDetails';
+//import TextComponent from './component';
+//import TodoComponent from './Todo';
+
+function App() {
+  return (
+    <div className="App">
+     <TeamDetails />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+// {/* <TextComponent  firstName="MOHAN" lastName="RAJ"
+//       age="23" /> */}
